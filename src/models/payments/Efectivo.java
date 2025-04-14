21dba75ee0aca874b637181f312439e2e5f3df16
Francomanx <143547506+Francomanx@users.noreply.github.com>
2025-04-13 20:25:37 -0400

    Delete src/models directory

--- src/models/payments/Efectivo.java
+++
@@ -1,9 +0,0 @@
-package models.payments;
-
-public class Efectivo extends Pago {
-
-    @Override
-    public void RealizarPago() {
-        System.out.println("Pago realizado con exito!");
-    }
-}
